3d4e8ff55d58ef7f6fcb23b625fa87e18d5c5095
Piotr <piotrektymula1@gmail.com>
2023-11-20 13:58:48 +0100

    finished progrma with parallel computing

---
+++ 4DatabaseProject/src/Employee.java
@@ -0,0 +1,8 @@
+
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    // Constructors, getters, setters, etc.
+}
--- Threads2/src/Simple_executor_test.java
+++ Threads2/src/Simple_executor_test.java
@@ -7,50 +7,59 @@ import java.util.concurrent.Future;
 
 public class Simple_executor_test {
 
-    private static final int NTHREADS = 10;
+    private static final int NTHREADS = 8;
         
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-
+    public static void main(String[] args)  {
 		double dx = 0.001;
-		double sum = 0.0;
+		double sum = 0.0, sum_seq = 0.0;
 
-		Counter counter = new Counter();
-		ExecutorService executor = Executors.newFixedThreadPool(NTHREADS);
+		try {
 
-		double my_stride = (Math.PI - 0) / 50;
-		double my_start = 0;
-		double my_end = my_stride;
+//		Counter counter = new Counter();
+			ExecutorService executor = Executors.newFixedThreadPool(NTHREADS);
 
-		List<Future<Double>> future_list = new ArrayList<>();
+			double my_stride = (Math.PI - 0) / 50;
+			double my_start = 0;
+			double my_end = my_stride;
 
-		for (int i = 0; i < 50; i++) {
-			Runnable worker = new CounterPlus(counter);
-			executor.execute(worker);
+			List<Future<Double>> future_list = new ArrayList<>();
 
-			Calka_callable calka1 = new Calka_callable(my_start, my_end, dx);
+			for (int i = 0; i < 50; i++) {
+//			Runnable worker = new CounterPlus(counter);
+//			executor.execute(worker);
 
-			my_start = my_end;
-			my_end += my_stride;
+				Calka_callable calka1 = new Calka_callable(my_start, my_end, dx);
+				sum_seq += calka1.compute_integral();
 
-			Future<Double> f = executor.submit(calka1);
-			future_list.add(f);
+				my_start = my_end;
+				my_end += my_stride;
+
+				Future<Double> f = executor.submit(calka1);
+				future_list.add(f);
+			}
+
+			for (Future<Double> x : future_list) {
+				double res = x.get();
+				sum += res;
+			}
+
+			// This will make the executor accept no new threads
+			// and finish all existing threads in the queue
+			executor.shutdown();
+
+			// Wait until all threads finish
+			while (!executor.isTerminated()) {
+			}
+
+			System.out.println("Calka seq: " + sum_seq);
+			System.out.println("Calka watki: " + sum);
+			System.out.println("Finished all threads");
+//		System.out.format("\nCounter_1: %d, Counter_2 %d\n\n",
+//				  counter.get_c1(), counter.get_c2());
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-
-		for(Future<Double> x : future_list) {
-			double res = x.get();
-			sum += res;
-		}
-
-		// This will make the executor accept no new threads
-		// and finish all existing threads in the queue
-		executor.shutdown();
-
-		// Wait until all threads finish
-		while (!executor.isTerminated()) {}
-
-		System.out.println("Calka watki: " + sum);
-		System.out.println("Finished all threads");
-		System.out.format("\nCounter_1: %d, Counter_2 %d\n\n",
-				  counter.get_c1(), counter.get_c2());
 	}
 } 
